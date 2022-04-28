223725cd098a7dbecccac687fd8e9f9de0137f00
Rodrigo Zepeda-Tello <rzepeda17@gmail.com>
2022-04-28 13:25:21 -0500

    Fixed osf error when listing files

--- upload_osf.R
+++ upload_osf.R
@@ -14,11 +14,11 @@ covid_project <- osf_retrieve_node("https://osf.io/9nu2d/")
 
 #List files
 osfiles <- covid_project %>%
-  osf_ls_files("Datos Abiertos COVID")
+  osf_ls_files("Datos Abiertos COVID", n_max = Inf)
 
 #Get address
 osfiles_address <- covid_project %>%
-  osf_ls_files(pattern = "Datos Abiertos Covid") 
+  osf_ls_files(pattern = "Datos Abiertos COVID") 
 
 for (fname in zipdata){
   if (!(str_remove(fname,"\\./") %in% osfiles$name)){
